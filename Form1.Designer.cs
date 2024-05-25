11fadda4a911f81de7649d165e085da66cf9886f
Juraj Borza <jborza@gmail.com>
2024-05-25 18:13:35 +0200

    added dificulty picker

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,10 @@
         {
             textBox1 = new TextBox();
             pictureBox1 = new PictureBox();
+            buttonAutofill = new Button();
             button1 = new Button();
+            comboBoxDifficulty = new ComboBox();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -51,22 +54,54 @@
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             // 
+            // buttonAutofill
+            // 
+            buttonAutofill.Location = new Point(915, 450);
+            buttonAutofill.Name = "buttonAutofill";
+            buttonAutofill.Size = new Size(112, 34);
+            buttonAutofill.TabIndex = 2;
+            buttonAutofill.Text = "Autofill";
+            buttonAutofill.UseVisualStyleBackColor = true;
+            buttonAutofill.Click += buttonAutofill_Click;
+            // 
             // button1
             // 
-            button1.Location = new Point(915, 450);
+            button1.Location = new Point(912, 490);
             button1.Name = "button1";
             button1.Size = new Size(112, 34);
-            button1.TabIndex = 2;
+            button1.TabIndex = 3;
             button1.Text = "Step";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // comboBoxDifficulty
+            // 
+            comboBoxDifficulty.FormattingEnabled = true;
+            comboBoxDifficulty.Items.AddRange(new object[] { "Easy", "Medium", "Hard" });
+            comboBoxDifficulty.Location = new Point(914, 598);
+            comboBoxDifficulty.Name = "comboBoxDifficulty";
+            comboBoxDifficulty.Size = new Size(182, 33);
+            comboBoxDifficulty.TabIndex = 4;
+            comboBoxDifficulty.SelectedIndexChanged += comboBoxDifficulty_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(916, 567);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 25);
+            label1.TabIndex = 5;
+            label1.Text = "Difficulty";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1137, 923);
+            Controls.Add(label1);
+            Controls.Add(comboBoxDifficulty);
             Controls.Add(button1);
+            Controls.Add(buttonAutofill);
             Controls.Add(pictureBox1);
             Controls.Add(textBox1);
             Name = "Form1";
@@ -80,6 +115,9 @@
 
         private TextBox textBox1;
         private PictureBox pictureBox1;
+        private Button buttonAutofill;
         private Button button1;
+        private ComboBox comboBoxDifficulty;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -8,30 +8,67 @@ namespace WinFormsApp1
         {
             InitializeComponent();
             initialize();
+            comboBoxDifficulty.SelectedIndex = 1;
         }
 
         private void initialize()
         {
-            string numbers = textBox1.Text;
-            int[,] sudoku = new int[9, 9]
+            
+        }
+
+        private void buttonAutofill_Click(object sender, EventArgs e)
         {
-            {0,4,9,0,0,0,0,3,0 },
-            {0,5,0,6,1,0,0,0,0 },
-            {0,0,8,0,2,9,5,0,6 },
-            {8,0,0,9,0,7,0,0,4 },
-            {7,0,0,0,0,0,0,8,1 },
-            {0,2,5,0,4,1,3,0,0 },
-            {2,0,0,0,7,6,0,1,0 },
-            {5,0,0,4,0,8,7,0,0 },
-            {0,8,7,0,0,0,0,9,5 }
-        };
-            grid = new Grid(sudoku);
+            grid.AutoFill();
             pictureBox1.Image = grid.Draw();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            grid.Step();
             grid.AutoFill();
+            pictureBox1.Image = grid.Draw();
+        }
+
+        private void comboBoxDifficulty_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBoxDifficulty.SelectedIndex == 0)
+            { 
+                int[,] sudoku = new int[9, 9]
+                {
+                    {0,4,9,0,0,0,0,3,0 },
+                    {0,5,0,6,1,0,0,0,0 },
+                    {0,0,8,0,2,9,5,0,6 },
+                    {8,0,0,9,0,7,0,0,4 },
+                    {7,0,0,0,0,0,0,8,1 },
+                    {0,2,5,0,4,1,3,0,0 },
+                    {2,0,0,0,7,6,0,1,0 },
+                    {5,0,0,4,0,8,7,0,0 },
+                    {0,8,7,0,0,0,0,9,5 }
+                };
+                grid = new Grid(sudoku);
+            }
+            else if(comboBoxDifficulty.SelectedIndex == 1)
+            {
+                //medium
+                int[,] sudoku = new int[9, 9]
+                {
+                    {0,9,4,0,5,0,7,0,0 },
+                    {0,3,5,0,8,2,0,0,4 },
+                    {0,0,1,0,0,0,0,5,0 },
+                    {0,0,2,0,0,5,0,0,0 },
+                    {4,0,0,1,0,3,0,0,6 },
+                    {0,0,0,0,0,0,3,7,0 },
+                    {0,0,0,0,0,0,2,0,0 },
+                    {5,6,0,8,2,0,0,9,7 },
+                    {0,0,9,0,7,0,4,0,3 }
+                };
+                grid = new Grid(sudoku);
+            }
+            else
+            {
+                //hard
+                //TODO fill 
+            }
             pictureBox1.Image = grid.Draw();
         }
     }
